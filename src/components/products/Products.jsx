45ec2a84f4b6934bd93fd9875ace136ca24e69cf
Martin Melo <nitram-210397@hotmail.com>
2019-02-27 11:37:40 -0600

    Storage working

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
-import { Icon, Button, Input, Col, Row } from 'antd'
-
+import { Icon, Button, Input, Col, Row, Progress } from 'antd'
+import firebase from 'firebase'
 class Products extends Component {
 
     constructor(props){
         super(props)
+        this.storage = firebase.storage()
         this.state = {
             newProduct: {
                 nombre: null,
@@ -12,12 +13,10 @@ class Products extends Component {
                 precio: null,
                 imagen: null,
             },
-            productos: null
+            productos: null,
+            percentage: null,
+            isUploading: false
         }
-    }
-
-    componentWillMount = () => {
-
     }
 
     changeName = (e) => {
@@ -38,7 +37,39 @@ class Products extends Component {
         this.setState({newProduct: obj})
     }
 
+    changeImage = (e) => {
+        const image = e.target.files[0]
+        const storageRef = firebase.storage().ref(`images/${image.name}`)
+        const task = storageRef.put(image)
+
+        task.on('state_changed', snapshot => {
+            
+            let percentage = (snapshot.bytesTransferred/snapshot.totalBytes) * 100
+            percentage = percentage.toFixed(2)
+            this.setState({isUploading: true, percentage})
+        },
+        error => {
+            console.log(error)
+        },
+        ()=>{
+            task.snapshot.ref.getDownloadURL()
+                .then(url=>{
+                    let obj = this.state.newProduct
+                    obj.imagen = url
+                    this.setState({newProduct: obj})
+                    alert("YA SE SUBIO LA PINCHE IMAGEN")
+                })
+                .catch(err=>alert("FALLO ESTA PENDEJADA"))
+        }
+        )
+    }
+
+
     uploadProduct = () => {
+           
+        //this.saveImageinStorage()
+        //Before save in DB
+
         this.props.db.collection('products').add(this.state.newProduct)
             .then((docRef)=>alert("El producto se ha creado"))
             .catch(err=>
@@ -83,13 +114,18 @@ class Products extends Component {
                                 value={this.state.newProduct.precio}
                                 onChange={(e)=>this.changePrecio(e)}
                             />
-                            <Button 
-                                type="primary"
-                                shape="round"
-                                icon="upload"
-                                size={"large"}
-                                onClick={this.uploadProduct}
-                            >Guardar producto</Button>
+                            <input type="file"
+                                onChange={(e)=>this.changeImage(e)}
+                                />
+                            {this.state.isUploading ?    
+                            <Progress type={"circle"} percent={this.state.percentage} />
+                            : null}
+                            <Button
+                            type="primary"
+                            icon="upload"
+                            size={"large"}
+                            onClick={this.uploadProduct}
+                            >Guardar producto </Button>
                         </Col>
                     </Row>
                 </Col>
